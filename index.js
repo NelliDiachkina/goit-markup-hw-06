5c8f082c1003d3b4344d21917f2f3e081fe1f68f
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-06-28 11:05:18 +0300

    update index.js

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const mobileNav = document.querySelector('.mobile-menu');
 
 heroBtn.addEventListener('click', () => toggleModal(modalWindow));
 burgerBtn.addEventListener('click', () => toggleModal(mobileNav));
-modalWindow.addEventListener('click', handleModalButtonClick);
-mobileNav.addEventListener('click', handleModalButtonClick);
+modalWindow.addEventListener('click', handleClick);
+mobileNav.addEventListener('click', handleClick);
 
 function toggleModal(element) {
   element.classList.toggle('is-open');
   document.documentElement.classList.toggle('scroll-disabled');
 }
 
-function handleModalButtonClick(e) {
+function handleClick(e) {
   if (e.target.closest('.modal-btn') || e.target.closest('.mobile-menu-link')) {
     toggleModal(e.currentTarget);
   } else if (e.currentTarget === modalWindow && e.target === modalWindow) {
